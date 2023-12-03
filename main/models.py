d03cbe87287d59f561bb9d2299eb4e1acdff375a
Adeleke Barakat <adelekebarakat96@gmail.com>
2023-12-03 22:00:45 +0100

    model changes

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = RichTextField()
     mini_description = models.TextField()
-    post_image = models.ImageField(upload_to='imgblog/', blank=True)
+    post_image = models.ImageField(upload_to='imgblog/')
     post_date = models.DateTimeField(auto_now_add=True)
     slug = models.CharField(max_length=1000, null=True, blank=True)
 
